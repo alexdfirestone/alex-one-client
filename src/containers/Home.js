5f6157a9b8cdafce6397963c25860cf639437bc3
Alex Firestone <alexfirestone@Alexs-MacBook-Pro-2.local>
2020-12-15 22:52:48 -0800

    add router

---
+++ src/containers/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+
+export default function Home(){
+    return(
+        <div className='Home'>
+            <div className='lander'>
+                <h1>Alex One Home</h1>
+                <p className='text-muted'>Welcome to the alex one home page</p>
+            </div>
+        </div>
+    );
+}
